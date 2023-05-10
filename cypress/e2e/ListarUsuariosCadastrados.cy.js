33873d5dd62aa23297a993f6c1d8fdebe707bea2
julia-iense <julia.iense@gmail.com>
2023-05-10 16:56:48 -0300

    atualização dos cenarios

--- cypress/e2e/ListarUsuariosCadastrados.cy.js
+++ cypress/e2e/ListarUsuariosCadastrados.cy.js
@@ -1,11 +1,10 @@
-/// <reference types="cypress" />
+/// <reference types="Cypress" />
+import {usersUrl} from '../support/commands.js'
+
 const headers = {
   'accept': 'application/json',
   'Content-Type': 'application/json'
 };
-
-const baseUrl = 'https://serverest.dev';
-const usersUrl = `${baseUrl}/usuarios`;
 
 const ListUser = (payload) => {
   return cy.request({
@@ -17,14 +16,46 @@ const ListUser = (payload) => {
   });
 };
 
-describe('lista usuários', () => {
-  it('lista usuários', () => {
+describe('Listar usuários cadastrados', () => {
+  it('Listar usuários com somente nome', () => {
     const payload = {
-      nome: "teste"
+      nome: 'generateRandomLetters'
     };
     ListUser(payload).then((response) => {
       expect(response.status).to.eq(200);
-      expect(response.body.message).to.equal('Cadastro realizado com sucesso');
+      expect(response.body.quantidade).to.have.property;
+      expect(response.body.usuarios).to.have.property
+    });
+  });
+  it('Listar usuários - email somente letras ', () => {
+    const payload = {
+      email: 'generateRandomLetters'
+    };
+    cy.log(payload)
+    ListUser(payload).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body.email).to.equal('email deve ser um email válido')
+    });
+  });
+  it('Listar usuários - email somente números', () => {
+    const payload = {
+      email: "1552968416515"
+    };
+    cy.log(payload)
+    ListUser(payload).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body.email).to.equal('email deve ser uma string')
+    });
+  });
+  it('Listar usuários - pesquisa por _id', () => {
+    const payload = {
+      _id: "generateRandomLetters",
+    };
+    cy.log(payload)
+    ListUser(payload).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body.quantidade).to.have.property;
+      expect(response.body.usuarios).to.have.property
     });
   });
 });
